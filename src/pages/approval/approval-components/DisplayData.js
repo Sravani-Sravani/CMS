d6700b2ad47bd682b6bcecc00d4d4bc6253602e4
Dey <chiranjitdey@kpmg.com>
2023-03-02 14:19:44 +0530

    dashboard done

--- src/pages/approval/approval-components/DisplayData.js
+++ src/pages/approval/approval-components/DisplayData.js
@@ -49,7 +49,19 @@ const rows = [
 ]  
 
 
+
 const DisplayData=()=>{
+    const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
   return (
     <>
       <Approval/>
@@ -100,6 +112,15 @@ const DisplayData=()=>{
                      </TableBody>
                 </Table>
             </TableContainer>
+            <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
         </CardContent>
       </Card>
     </>
